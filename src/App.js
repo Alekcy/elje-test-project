2527d10a37018e8dcf26d8d8855aa7a93f493255
Unknown <alekcyvolkov@gmail.com>
2019-08-04 17:48:42 +0300

    feat: add images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: black;
+`;
+
+const Image = styled.img`
+    max-width: 100%;
+    max-height: 100%;
+    margin: auto;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    position: absolute;
+`;
+
+function getImageSource() {
+  return window.innerWidth > window.innerHeight
+    ? 'https://static.aviapark.com/files/155333/960x576_1.png'
+    : 'https://static.aviapark.com/files/155333/nc_ss19_620x960px_1.png';
+}
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Image src={getImageSource()} />
+    </Container>
   );
 }
 
